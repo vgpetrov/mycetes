b4be830b8df0e50f237590628b36d4f5c0e7658d
vitaly.petrov <vitaly.petrov@piano.io>
2025-08-04 23:27:05 +0400

    init stuff

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,4 +1,53 @@
+use axum::Router;
+use axum::extract::Path;
+use axum::response::Html;
+use axum::routing::get;
+use std::sync::Arc;
+use statsd::Client;
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let statsd_client = Client::new("statsd:8125", "axum_hello.request").unwrap();
+    let arc_statsd = Arc::new(statsd_client);
+
+    let app = Router::new()
+        .route("/", get(hello_handler))
+        .route("/:name", get(move |name| named_handler(name)))
+        .route(
+            "/stats1/:name",
+            get({
+                let arc = Arc::clone(&arc_statsd);
+                move |name| named_handler_stats(name, arc)
+            }),
+        )
+        .route(
+            "/stats2/:name",
+            get({
+                let arc = Arc::clone(&arc_statsd);
+                move |name| named_handler_stats(name, arc)
+            }),
+        );
+
+    let listener = tokio::net::TcpListener::bind(("0.0.0.0", 3002))
+        .await
+        .unwrap();
+
+    println!("listening on {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app).await.unwrap()
+}
+
+async fn hello_handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
+}
+
+async fn named_handler(Path(name): Path<String>) -> Html<String> {
+    let s = format!("<h1>Hello, {name}!</h1>");
+    Html(s)
+}
+
+async fn named_handler_stats(Path(name): Path<String>, statsd: Arc<Client>) -> Html<String> {
+    statsd.incr("count");
+    let s = format!("<h1>This is stats, {name}!</h1>");
+    Html(s)
 }
